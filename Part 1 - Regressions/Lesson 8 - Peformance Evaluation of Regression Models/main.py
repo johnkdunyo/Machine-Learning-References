1fdadc1cbd5ffa6b10acecd263b2ca8b93a1340d
johnkdunyo <johnkdunyo@gmail.com>
2022-09-04 16:51:00 +0000

    feature: performance evaluation of regression models as lesson 8

---
+++ Part 1 - Regressions/Lesson 8 - Peformance Evaluation of Regression Models/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 16:29:13 2022
+
+@author: jondexter
+"""
+
+'''
+Here, we evaluate the performanc of all our regression models using key metrics
+such as R-squared
+
+
+R^2 = 1 - (sum of residuals)/(total sum of squares)
+
+the closer it is to 1, the better the model
+
+
+Adjusted R-Squared
+'''
